943d176cfc98750a6c10795c437e0a5072cafb2a
LoyolaTejeda <ca.loyola.tejeda@gmail.com>
2021-10-11 22:48:49 -0400

    Header listo

---
+++ dependencias.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="librerias/normalize.css">
+<link rel="stylesheet" href="librerias/css/bootstrap.css">
+<link rel="stylesheet" href="librerias/master.css">
+<link rel="stylesheet" href="librerias/contenido.css">
+
+
+<script src="librerias/js/bootstrap.js"></script>
---
+++ headerOriontek.php
@@ -0,0 +1,39 @@
+<header>
+    <nav class="navbar navbar-expand-md navbar-light bg-light btn-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand btn-close-white" href="#"><img src="recursos/img/LogoOriontek.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+                    aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav me-auto my-2 my-lg-2 " style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Empresas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Clientes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Portafolio</a>
+                    </li>
+                </ul>
+                <section class="d-flex bg-light">
+                    <div class="form-control toast-header justify-content-center">
+                        <label class="me-lg-3" for="">REDES</label>
+
+
+                        <a class="me-lg-1 m-3" href="#"><img src="recursos/Icon/whatsapp.png" alt="WhatsApp" title="WhatsApp"></a>
+                        <a class="me-lg-1 m-3" href="#"><img src="recursos/Icon/linkedin-icon-32x32.png" alt="Linkidin" title="Linkedin"></a>
+                        <a class="me-lg-1 m-3" href="#"><img src="recursos/Icon/email-icon-32x32.png" alt="Email" title="Email"></a>
+                        <a class="me-lg-1 m-3" href="#"><img src="recursos/Icon/facebook%2032.png" alt="FaceBook" title="FaceBook"></a>
+                    </div>
+                </section>
+
+            </div>
+        </div>
+    </nav>
+</header>
+
+<div  class="header p-1 bg-opacity-10">
+</div>
