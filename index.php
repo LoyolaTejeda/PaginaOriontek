e0bbe20d79ac8f3148c56c2e669ecac816c0deb2
LoyolaTejeda <ca.loyola.tejeda@gmail.com>
2021-10-11 22:51:08 -0400

    Pagina de inicio lista, Faltan los enlaces

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Oriontek</title>
+    <?php     require_once ("dependencias.php");   ?>
+    <!-- -->
+</head>
+
+<body style="background-image: url(recursos/img/portada.png)"> <!-- Aplico un fonde de pantalla-->
+
+<?php require_once ("headerOriontek.php");  ?> <!-- Introduzco el header de la pagina-->
+
+
+<section class="sobreNosotros"><!-- Seccion sobre nosotros del inicio-->
+<div class="card container contenedor">
+
+    <div class="card-header">
+        <div class="card-title">
+            <h2 class="titulo">SOBRE NOSOTROS</h2>
+        </div>
+    </div>
+
+    <div class="card-body">
+        <p class="parrafo">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, alias aliquam aliquid aperiam aut, deserunt doloribus eius iusto maiores quae quo sit voluptates. Animi, ducimus labore maxime perferendis perspiciatis voluptatem?
+        </p>
+        <p class="parrafo">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consequuntur corporis cum excepturi id laudantium, molestiae nam, porro, quibusdam tenetur vero voluptate! Error eveniet excepturi nostrum praesentium rerum, tempore vel.
+        </p>
+        <p class="parrafo">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam culpa eos obcaecati, omnis qui recusandae repellat sed! Beatae, blanditiis dolor in minima neque officia pariatur perspiciatis, quidem recusandae totam, voluptas.
+        </p>
+    </div>
+
+    <div class="card-footer">
+        <div class="card-subtitle">
+            <p>Este es el subTitulo</p>
+        </div>
+    </div>
+</div>
+</section>
+
+</body>
+</html>
